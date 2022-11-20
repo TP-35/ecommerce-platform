3a9cdf128adbe3f43e2a4d5a465db2cd011d1c25
REMY THOMPSON <remerdy1@gmail.com>
2022-11-20 00:54:03 +0000

    Replace template literal with placeholders to prevent SQL injection

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,10 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Sign-up Route
-router.post("/signup", async (req, res) =>{
-    try{
-        let {email, username, password, confirmPassword} = req.body;
-        
+router.post("/signup", async (req, res) => {
+    try {
+        let { email, username, password, confirmPassword } = req.body;
+
         // Remove whitespace
         email = email.trim();
         username = username.trim();
@@ -21,54 +21,53 @@ router.post("/signup", async (req, res) =>{
 
         // Validate Email
         const emailIsValid = validator.validate(email);
-        if(!emailIsValid) return res.status(400).send("Email is invalid.");
+        if (!emailIsValid) return res.status(400).send("Email is invalid.");
 
         // Check if email already exists
-        const [emailMatch] = await db.execute(`SELECT * FROM user WHERE email="${email}"`);
+        const [emailMatch] = await db.execute(`SELECT * FROM user WHERE email=?`, [email]);
         const emailMatchUser = emailMatch[0];
-        if(emailMatchUser) return res.status(400).send("There is already an account attached to this email.");
-        
+        if (emailMatchUser) return res.status(400).send("There is already an account attached to this email.");
+
         // Check if username already exists
-        const [usernameMatch] = await db.execute(`SELECT * FROM user WHERE username="${username}"`);
+        const [usernameMatch] = await db.execute(`SELECT * FROM user WHERE username=?`, [username]);
         const usernameMatchUser = usernameMatch[0];
-        if(usernameMatchUser) return res.status(400).send("Username is taken.");
-        
+        if (usernameMatchUser) return res.status(400).send("Username is taken.");
+
         // Check if username and password are the same
         if (password === username) return res.status(400).send("Username and Password cannot match");
-        
+
         // Validate Password (Capital letter, number, special character, 8 characters)
         const regex = /^(?=.*[A-Z])^(?=.*[0-9])(?=.*[\[\]£!@#\$%\^\&*\)\(+=._-])[a-zA-Z0-9\[\]£!@#\$%\^\&*\)\(+=._-]{8,}$/;
         const result = regex.test(password);
-        if(!result) return res.status(400).send("Password must be at least 8 characters long and contain at least 1 capital letter, 1 number and 1 special character");
+        if (!result) return res.status(400).send("Password must be at least 8 characters long and contain at least 1 capital letter, 1 number and 1 special character");
 
         // Check passwords match
-        if(password !== confirmPassword) return res.status(400).send("Passwords do not match.");
-        
+        if (password !== confirmPassword) return res.status(400).send("Passwords do not match.");
+
         // Hash password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         // Save user to database
-        await db.execute(`INSERT INTO user (email, username, password) VALUES ("${email}","${username}","${hashedPassword}");`);
+        await db.execute(`INSERT INTO user (email, username, password) VALUES (?, ?, ?);`, [email, username, hashedPassword]);
 
         //Create web token 
-        const token = await jwt.sign({data: username}, process.env.SECRET, { expiresIn: '24h' });
-        
+        const token = await jwt.sign({ data: username }, process.env.SECRET, { expiresIn: '24h' });
+
         return res.send(token);
-    } 
-    catch(e){
+    } catch (e) {
         console.log(e);
         return res.status(500).send();
     }
 })
 
 // Login Route
-router.post("/login", async (req, res) =>{
-    try{
-        const {username, password} = req.body;
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
         // Find user
-        const [row] = await db.execute(`SELECT * FROM user WHERE username="${username}"`);
+        const [row] = await db.execute(`SELECT * FROM user WHERE username=?`, [username]);
         const user = row[0];
-        if(!user){
+        if (!user) {
             return res.status(400).send();
         }
 
@@ -76,15 +75,15 @@ router.post("/login", async (req, res) =>{
         const match = await bcrypt.compare(password, user.password);
         console.log(match);
 
-        if(!match){
+        if (!match) {
             return res.status(400).send("Passwords do not match.");
         }
 
         // Create web token
-        const token = await jwt.sign({data: username}, process.env.SECRET, { expiresIn: '24h' });
-        
+        const token = await jwt.sign({ data: username }, process.env.SECRET, { expiresIn: '24h' });
+
         res.send(token);
-    }catch(e){
+    } catch (e) {
         console.log(e);
         return res.status(500).send();
     }
