926d79ad65529f2d9eabce528aa8fdabaef331d4
REMY THOMPSON <remerdy1@gmail.com>
2022-11-26 19:08:41 +0000

    Basic navbar. Check if user signed in on every route

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,32 +5,73 @@ const jwt = require("jsonwebtoken");
 //todo Render each page
 router.get("/", async (req, res) => {
     let token;
-    
+
     if (req.cookies.token) {
         try {
             token = await jwt.verify(req.cookies.token, process.env.SECRET);
-        } catch(e) {
+        } catch (e) {
             token = null;
         }
     }
-    
-    res.render("index.ejs", {token});
+
+    res.render("index.ejs", { token });
 })
 
-router.get("/aboutus", (req, res) => {
-    res.render("aboutus.ejs");
+router.get("/aboutus", async (req, res) => {
+    let token;
+
+    if (req.cookies.token) {
+        try {
+            token = await jwt.verify(req.cookies.token, process.env.SECRET);
+        } catch (e) {
+            token = null;
+        }
+    }
+
+    res.render("aboutus.ejs", { token });
 })
 
-router.get("/contactus", (req, res) => {
-    res.render("contactus.ejs");
+router.get("/contactus", async (req, res) => {
+    let token;
+
+    if (req.cookies.token) {
+        try {
+            token = await jwt.verify(req.cookies.token, process.env.SECRET);
+        } catch (e) {
+            token = null;
+        }
+    }
+
+    res.render("contactus.ejs", { token });
 })
 
-router.get("/login", (req, res) => {
-    res.render("login.ejs");
+router.get("/login", async (req, res) => {
+    let token;
+
+    if (req.cookies.token) {
+        try {
+            token = await jwt.verify(req.cookies.token, process.env.SECRET);
+            return res.render("/", { token });
+        } catch (e) {
+            token = null;
+        }
+    }
+    res.render("login.ejs", { token });
 })
 
-router.get("/signup", (req, res) => {
-    res.render("signup.ejs");
+router.get("/signup", async (req, res) => {
+    let token;
+
+    if (req.cookies.token) {
+        try {
+            token = await jwt.verify(req.cookies.token, process.env.SECRET);
+            return res.render("/", { token });
+        } catch (e) {
+            token = null;
+        }
+    }
+
+    res.render("signup.ejs", { token });
 })
 
 module.exports = router;
