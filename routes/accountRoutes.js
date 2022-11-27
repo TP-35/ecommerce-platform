408351ffbc9d22e95da7e9160451f90032d3bd33
Winter <thetruewinter@gmail.com>
2022-11-27 07:46:21 +0000

    Implemented list all users

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const db = require("../db");
 const router = express.Router();
 const auth = require("../middleware/auth.js");
+const adminAuth = require("../middleware/adminauth.js");
 
 /* Update user account
     todo verify user is logged in (auth middleware)
@@ -10,35 +11,35 @@ const auth = require("../middleware/auth.js");
     ? Change username or email 
 */
 
-router.patch("/:username", auth, async (req, res) =>{
-    try{
-        const {username} = req.params;
-        let {password, confirmPassword} = req.body;
-        
+router.patch("/:username", auth, async (req, res) => {
+    try {
+        const { username } = req.params;
+        let { password, confirmPassword } = req.body;
+
         // Remove whitespace
         password = password?.trim() || "";
         confirmPassword = confirmPassword?.trim() || "";
-    
+
         // Make sure form was filled
         if (!password || !confirmPassword) return res.status(400).send("Please fill the form.");
-    
+
         // Validate Password (Capital letter, number, special character, 8 characters)
         const regex = /^(?=.*[A-Z])^(?=.*[0-9])(?=.*[\[\]£!@#\$%\^\&*\)\(+=._-])[a-zA-Z0-9\[\]£!@#\$%\^\&*\)\(+=._-]{8,}$/;
         const result = regex.test(password);
         if (!result) return res.status(400).send("Password must be at least 8 characters long and contain at least 1 capital letter, 1 number and 1 special character");
-    
+
         // Check passwords match
         if (password !== confirmPassword) return res.status(400).send("Passwords do not match.");
-    
+
         // Hash password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-        
+
         // Update database
         await db.execute(`UPDATE user SET password = ? WHERE username = ?;`, [hashedPassword, username]);
-    
+
         return res.send();
-    }catch(e){
+    } catch (e) {
         console.log(e);
         res.status(500).send();
     }
@@ -47,16 +48,41 @@ router.patch("/:username", auth, async (req, res) =>{
 /*  Delete user account
     todo verify user is logged in (auth middleware)
 */
-router.delete("/:username", auth, async (req, res) =>{
-    try{
-        const {username} = req.params;
+router.delete("/:username", auth, async (req, res) => {
+    try {
+        const { username } = req.params;
         // remove user from database
         db.execute("DELETE FROM user WHERE username = ?;", [username]);
         res.send();
-    }catch(e){
+    } catch (e) {
         console.log(e);
         res.send(500);
     }
 })
+
+// List all Users route (requires admin)
+router.get("/users", adminAuth, async (req, res) => {
+    try {
+        const [users_rows] = await db.execute(`SELECT * FROM user ORDER BY user_id;`);
+        const users = users_rows[0];
+
+        // Returns an error if there are no users currently stored in the table
+        if (!users)
+            return res.status(400).send("There are currently no users in this table.");
+
+        // All users are added to the end of the array
+        let users_list = []
+        users_rows.forEach(user => {
+            users_list.push(user);
+        })
+
+        // Returns the list of users
+        return res.send(users_list);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send();
+    }
+})
+
 
 module.exports = router;
