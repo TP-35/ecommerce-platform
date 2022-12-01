710d623888438c545af3f83fd6d92fd03592dd27
Winter <thetruewinter@gmail.com>
2022-12-01 21:23:21 +0000

    Added checkout implementation

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,27 +1,59 @@
-const fullname = document.getElementById("full-name");
+
+const fullname = document.getElementById("fullname");
 const email = document.getElementById("email");
 const city = document.getElementById("city");
 const address = document.getElementById("address");
 const postcode = document.getElementById("postcode");
 
-(async () =>{
-    try{
+(async () => {
+    try {
         const res = await fetch("/users");
         const data = await res.json();
-        
+
         fullname.value = data.fullname;
         email.value = data.email;
         city.value = data.city;
         address.value = data.address;
         postcode.value = data.postcode;
-    }catch(e){
+    } catch (e) {
     }
 })()
 
-async function onCheckout(e){
-    e.preventDefault();
+const onCheckout = async (e) => {
+    e.preventDefault(); // Prevents the submit action, which will stop the page redirecting
 
-    
-    //todo save order to database
-    //todo clear basket to simulate purchase 
+    // Stores all the necessary values 
+    const fullname = e.target.fullname.value;
+    const email = e.target.email.value;
+    const address = e.target.address.value;
+    const city = e.target.city.value;
+    const postcode = e.target.postcode.value;
+    const quantity = 1;
+
+    // Tries to call the method that handles creating an order
+    try {
+        const res = await fetch("/orders", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                fullname,
+                email,
+                address,
+                city,
+                postcode,
+                quantity
+            })
+        })
+
+        // Returns the message if necessary
+        const data = await res.json();
+
+        // Redirects the user as intended
+        window.location.href = "http://localhost:3000/";
+    } catch (e) {
+        console.log(e);
+    }
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -151,58 +151,73 @@ router.get("/orders", auth, async (req, res) => {
 })
 
 // Create a new Order route (requires admin)
-router.post("/orders/:id", adminAuth, async (req, res) => {
-
+router.post("/orders", adminAuth, async (req, res) => {
     try {
-        let { user_id, address, postcode, order_total, quantity } = req.body;
+
+        let { address, city, postcode, quantity } = req.body;
 
         address = address?.trim() || "";
+        city = city?.trim() || "";
         postcode = postcode?.trim() || "";
+        postcode.replace(" ", "");
 
-        // Checks if the order total is a valid float value
-        if (isNaN(parseFloat(order_total)))
-            return res.status(400).send({ message: "The inputted order total must be a valid float value." });
-
-        if (!Number.isInteger(parseInt(quantity)))
+        if (!Number.isInteger(parseInt(quantity))) {
+            console.log("2");
             return res.status(400).send({ message: "The inputted quantity must be a valid integer value." });
+        }
 
         // Checks if all fields have been filled
-        if (!address || !postcode || !order_total)
+        if (!address || !postcode || !city) {
+            console.log("3");
             return res.status(400).send({ message: "Please fill in the form." });
+        }
+
+        // Validates postcode
+        const postcode_regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?[0-9][A-Z]{2}$/i;
+        //const postcode_result = postcode_regex.test(postcode);
+        //if (!postcode_result) {
+        //    console.log("Fail " + postcode);
+        //    return res.status(400).send({ message: "You have inputted an invalid postcode." });
+        //}
+
+        // Gets currently logged in user
+        const username = req.token.user.username;
+
+        // Finds user from username
+        const [user_row] = await db.execute(`SELECT * FROM user WHERE username=?;`, [username]);
+        let user = user_row[0];
+        let user_id = user.user_id;
+
+        // Returns an error if the username is not valid for any existing user
+        if (!user)
+            return res.status(400).send({ message: "This user could not be found." });
+
+        const [basket_rows] = await db.execute(`SELECT * FROM basket WHERE user_id=?;`, [user_id]);
+        const basket = basket_rows[0];
+
+        if (!basket)
+            return res.status(400).send({ message: "This user does not have a basket. " });
+
+        let order_total = 0;
+        for (const pr of basket_rows) {
+            const [product_rows] = await db.execute(`SELECT * FROM product WHERE product_id=?`, [pr.product_id]);
+            let product = product_rows[0];
+            order_total += parseFloat(product.cost) + parseFloat(product.shipping_cost);
+        }
 
         // Ensures the total is in correct format
         order_total = (Math.round(order_total * 100) / 100).toFixed(2);
 
-        // Validates postcode
-        const postcode_regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?[0-9][A-Z]{2}$/i;
-        const postcode_result = postcode_regex.test(postcode);
-        if (!postcode_result)
-            return res.status(400).send({ message: "You have inputted an invalid postcode." });
-
-        const product_id = req.params.id;
-
-        // Finds product from ID
-        const [product_rows] = await db.execute(`SELECT * FROM product WHERE product_id=?;`, [product_id]);
-        const product = product_rows[0];
-
-        // Returns an error if the ID is not valid for any existing product
-        if (!product)
-            return res.status(400).send({ message: "A product with this ID could not be found." });
-
-        // Gets user from user id
-        const [user_rows] = await db.execute(`SELECT * FROM user WHERE user_id=?`, [user_id]);
-        const user = user_rows[0];
-
-        // Returns an error if the user is not found
-        if (!user)
-            return res.status(400).send({ message: "A user with this ID could not be found." });
-
         // Creates a new order entry, using the user_id and product_id and stores the order_id
         const order = await db.execute("INSERT INTO `order` (user_id, product_id, order_date, address, postcode, order_total) VALUES (?, ?, ?, ?, ?, ?);",
-            [user_id, product_id, new Date(), address, postcode, order_total]);
+            [user_id, basket.product_id, new Date(), address, postcode, order_total]);
         const order_id = order[0].insertId;
 
-        await db.execute(`INSERT INTO order_item (product_id, order_id, quantity) VALUES (?, ?, ?);`, [product_id, order_id, quantity])
+        // All products from the basket are added to the end of the array (even if there is only one, for consistency)
+        for (const product of basket_rows) {
+            await db.execute(`INSERT INTO order_item (product_id, order_id, quantity) VALUES (?, ?, ?);`, [product.product_id, order_id, quantity]);
+        }
+
 
         // Returns the order id, which can be queried using GET orders/:id 
         return res.send(order_id.toString());
